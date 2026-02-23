973bc9083e1982a1f0be355ec3ae929d2fb30193
mkivanc27-boop <mkivanc27@gmail.com>
2026-02-23 21:40:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-import { GoogleAuthProvider, signInWithRedirect, getRedirectResult } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore, collection, addDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, signInWithRedirect } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyC98wxJQk8yNZFdE-OJ1Tlpy1ANuaRUT14",
+apiKey: "BURAYA_API_KEY",
 authDomain: "turkcord-47b24.firebaseapp.com",
 projectId: "turkcord-47b24",
 storageBucket: "turkcord-47b24.firebasestorage.app",
@@ -16,27 +15,44 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-let currentUser="";
+let currentUser = "";
+
+/* ===== EMAIL REGISTER ===== */
 
 window.register = async()=>{
 const email=document.getElementById("email").value;
 const pass=document.getElementById("password").value;
+
 await createUserWithEmailAndPassword(auth,email,pass);
 };
+
+/* ===== EMAIL LOGIN ===== */
 
 window.login = async()=>{
 const email=document.getElementById("email").value;
 const pass=document.getElementById("password").value;
+
 await signInWithEmailAndPassword(auth,email,pass);
 };
 
+/* ===== GOOGLE LOGIN ===== */
+
+window.googleLogin = async()=>{
+const provider = new GoogleAuthProvider();
+await signInWithRedirect(auth, provider);
+};
+
+/* ===== AUTH STATE ===== */
+
 onAuthStateChanged(auth,(user)=>{
 if(user){
-currentUser=user.email;
+currentUser = user.email;
 document.getElementById("auth").style.display="none";
 document.getElementById("app").style.display="flex";
 }
 });
+
+/* ===== SEND MESSAGE ===== */
 
 window.sendMessage = async()=>{
 const input=document.getElementById("messageInput");
@@ -51,9 +67,12 @@ time:new Date()
 input.value="";
 };
 
+/* ===== LOAD MESSAGES ===== */
+
 onSnapshot(collection(db,"messages"),(snap)=>{
 const div=document.getElementById("messages");
 div.innerHTML="";
+
 snap.forEach(doc=>{
 const data=doc.data();
 const msg=document.createElement("div");
@@ -62,12 +81,3 @@ msg.innerHTML=`<b>${data.user}</b><br>${data.text}`;
 div.appendChild(msg);
 });
 });
-
-    alert(error.message);
-  }
-};
-  if(result?.user){
-    console.log("Google login başarılı");
-  }
-});
-;
